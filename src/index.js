48487481a1fa7509364ea9295c38ba727e5c9468
Smartin Cherian <smartin.cherian@gmail.com>
2022-11-26 22:48:44 +0530

    jobassignment

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore }  from 'redux';
+// import birdsApp from './store/birds/birds';
+import jsonDataApp from './store/birds/jsonData';
+
+// const store = createStore(birdsApp);
+const store = createStore(jsonDataApp);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
